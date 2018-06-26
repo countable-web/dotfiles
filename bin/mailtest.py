8911142c51a67a7c4db8ada39918ac70b1ccbe3c
Clark Van Oyen <clark@countable.ca>
2018-06-25 17:50:00 -0700

    ok

--- bin/mailtest.py
+++ bin/mailtest.py
@@ -2,9 +2,13 @@ import sys
 def send_email():
     import smtplib
 
-    gmail_user = sys.argv[1]
-    gmail_pwd = sys.argv[2]
-    FROM = "catch@this.fish"
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    if len(sys.argv) > 3:
+        server_name = sys.argv[3]
+    else:
+        server_name = 'sendgrid'
+    FROM = "no-reply@bawkbox.com"
     TO = ["clark@countable.ca"]
     SUBJECT = "hello"
     TEXT = "hi"
@@ -12,11 +16,15 @@ def send_email():
     # Prepare actual message
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
     """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
-    server = smtplib.SMTP("email-smtp.ca-central-1.amazonaws.com", 587)
-    #server = smtplib.SMTP("smtp.sendgrid.net", 587)
+    if server_name == "aws":
+        server = smtplib.SMTP("email-smtp.ca-central-1.amazonaws.com", 587)
+    elif server_name == 'sendgrid':
+        server = smtplib.SMTP("smtp.sendgrid.net", 587)
+    else:
+        server = server_name
     server.ehlo()
     server.starttls()
-    server.login(gmail_user, gmail_pwd)
+    server.login(user, pwd)
     server.sendmail(FROM, TO, message)
     server.close()
 
