20c9e28026e5cdc1c037ca6e0eb355071bf1c95d
Clark Van Oyen <clark@countable.ca>
2018-06-28 04:36:54 +0000

    betterify it

--- bin/pg-backup-s3.sh
+++ bin/pg-backup-s3.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+set -e
+set -x
+
 site=$1
-filename=$site.$(date +%Y%m%d-%H%M%S).sql.lrz
+filename=$site.$(date +%Y%m%d-%H%M%S).sql
 aws=/home/clark/.local/bin/aws
 
-docker exec -t ${site}_db_1 pg_dump -U postgres postgres | lrzip > /tmp/$filename
-$aws s3 mv /tmp/$filename s3://countable/backups/$site/$filename
+docker exec -it ${site}_db_1 pg_dump -U postgres -f /tmp/db.sql postgres
+docker cp ${site}_db_1:/tmp/db.sql /tmp/$filename
+lrzip /tmp/$filename
+$aws s3 mv /tmp/$filename.lrz s3://countable/backups/$site/$filename.lrz
 
