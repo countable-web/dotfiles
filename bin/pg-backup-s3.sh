fb0e4c3d584cdb9e47be6d7079792c60b97fcbf0
Clark Van Oyen <clark@countable.ca>
2018-10-22 11:45:23 -0700

    force cleanup

--- bin/pg-backup-s3.sh
+++ bin/pg-backup-s3.sh
@@ -6,8 +6,8 @@ set -x
 site=$1
 filename=$site.$(date +%Y%m%d-%H%M%S).sql
 folder=$(date +%Y%m)
-rm /tmp/*.sql
-rm /tmp/*.lrz
+rm -f /tmp/*.sql
+rm -f /tmp/*.lrz
 docker exec ${site}_db_1 pg_dump -U postgres -f /tmp/db.sql postgres
 docker cp ${site}_db_1:/tmp/db.sql /tmp/$filename
 lrzip /tmp/$filename
