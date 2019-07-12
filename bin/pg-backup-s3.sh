d34ccef61bc46fdc4ef16e64dc9027de1db45527
Clark Van Oyen <clark@countable.ca>
2019-07-12 13:31:52 -0700

    new script

--- bin/pg-backup-s3.sh
+++ bin/pg-backup-s3.sh
@@ -9,6 +9,6 @@ filename=$site.$(date +%Y%m%d-%H%M%S).sql
 docker exec ${site}_db_1 pg_dump -U postgres -f /tmp/db.sql postgres
 docker cp ${site}_db_1:/tmp/db.sql /tmp/$filename
 lrzip /tmp/$filename
-rm /tmp/$filename
-aws s3 mv /tmp/$filename.lrz s3://${2}$site/$filename.lrz
+#rm /tmp/$filename
+#aws s3 mv /tmp/$filename.lrz s3://${2}$site/$filename.lrz
 
